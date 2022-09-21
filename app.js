aed6e8cfc24bcb3c3aede42e941a8c2e67c6f236
Andrew Cornejo <atcornejo3@gmail.com>
2022-09-21 12:21:01 -0700

    committing for turn in. working on JS functionality

--- app.js
+++ app.js
@@ -9,16 +9,61 @@ const sayGoodbyeButton = document.getElementById('say-goodbye-button');
 const monstersSection = document.getElementById('monsters-section');
 /* State */
 let message = `The mage portal has landed you in the middle of Goblin Territory! Fight your way out!`;
-// let roll = type: '1 hit', type: '1 hit', type: '1 hit' , type: '2 hit' ,type: '3 hit',
+let roll = [{type: '0hit', type: '1hit', type: '1hit' , type: '2hit' , type: '3hit'}]
 let monsters = [
-    { name: 'Larx', hitpoints: 0 },
-    { name: 'Barx', hitpoints: 0 },
-    { name: 'Yarx', hitpoints: 0 },
+    { name: 'Larx', hitpoints: 3, type: goblin },
+    { name: 'Barx', hitpoints: 3, type: goblin },
+    { name: 'Yarx', hitpoints: 3, type: goblin },
 ];
 
 // static types and probabilites
+const 0Hit = {
+    type: '0hit',
+    value: 0, 
+};
+const 1Hit = {
+    type: '1hit',
+    value: 1, 
+};
+const 2Hit = {
+    type: '2hit',
+    value: 2, 
+};
+const 3Hit = {
+    type: '3hit',
+    value: 3, 
+};
+
+const totalHit = [0, 1, 1, 2, 3];
+const hitTypeFound = [0hit, 1hit, 1hit, 2hit, 3hit];
+
+
 
 /* Events */
+const hitMessage = ['You swing and Miss!', 'Your blade found 1 hit', 'Your blade hit 2 times', 'You slash for 3 quick hits!'];
+
+huntMonstersButton.addEventListener('click', () => {
+    const found = getRandomItem(totalHit);
+
+    for (let i = 0, i < found; i++) {
+        //get a random attack value
+        const rollType = getRandomItem(hitTypeFound);
+
+        const roll = {
+            type: rollType.type,
+        };
+        
+        rolls.push(roll);
+    }
+
+    message = hitMessage[found];
+
+    displayMessage();
+    displayRolls();
+});
+
+add addMonsterForm
+
 
 /* Display Functions */
 
